8af1d403a6e1e78c21afd10bd6bc62bd3cd126a0
Tommy Murphy <tommy@ourbunny.com>
2013-04-24 21:34:56 -0500

    limit basedir log
    only print basedir once from main pg execution instead of each getpath
    instance

--- res.py
+++ res.py
@@ -23,11 +23,14 @@
 import os
 import sys
 
-def getpath(name):
+def getpath(name=None):
     if getattr(sys, '_MEIPASS', None):
         basedir = sys._MEIPASS
     else:
         #basedir = os.path.dirname(__file__)
         basedir = os.getcwd()
+        
+    if name is None:
+        return basedir
 
     return os.path.join(basedir, name)
